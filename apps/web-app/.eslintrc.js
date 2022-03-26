70e66c04102f801d6a46361bc3907b842cbb462d
Asaf <asaf.karavani1@gmail.com>
2022-03-26 03:25:23 +0300

    router

--- apps/web-app/.eslintrc.js
+++ apps/web-app/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/vite.config.ts'] }],
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-one-expression-per-line': 'off',
   },
   settings: {
     'import/resolver': {
--- apps/web-app/src/main.tsx
+++ apps/web-app/src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<> </>} />
+        <Route path="invoices" element={<> </>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
