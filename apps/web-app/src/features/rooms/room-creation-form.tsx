a521b58a24742433a2d30c46bb5453af967daed5
Assaf Koren <asaf.karavani1@gmail.com>
2022-03-28 19:54:18 +0300

    .

--- apps/web-app/src/features/rooms/room-creation-form.tsx
+++ apps/web-app/src/features/rooms/room-creation-form.tsx
@@ -1,11 +1,9 @@
-import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
+import { FormControl, OutlinedInput } from '@mui/material';
 
 export function RoomCreationForm() {
   return (
     <FormControl>
-      <InputLabel htmlFor="my-input">Email address</InputLabel>
-      <Input id="my-input" aria-describedby="my-helper-text" />
-      <FormHelperText id="my-helper-text">Well never share your email.</FormHelperText>
+      <OutlinedInput placeholder="Enter room name" />
     </FormControl>
   );
 }
