34454d497c1021392b9b7a96f640c6a59f2423df
Asaf <asaf.karavani1@gmail.com>
2022-04-02 01:56:42 +0300

    styles

--- apps/web-app/src/features/layout/header.tsx
+++ apps/web-app/src/features/layout/header.tsx
@@ -1,18 +1,12 @@
-import { Menu } from '@mui/icons-material';
-import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
+import { Box, AppBar, Toolbar } from '@mui/material';
+import logo from '../../assets/logo.png';
 
 export function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <Menu />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+        <Toolbar style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src={logo} alt="scrum.bet logo" style={{ height: 18 }} />
         </Toolbar>
       </AppBar>
     </Box>
--- apps/web-app/src/features/rooms/room-creation-form.tsx
+++ apps/web-app/src/features/rooms/room-creation-form.tsx
@@ -1,10 +1,18 @@
 import { Button, FormControl, OutlinedInput } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import Color from 'color';
+import './room-creation-form.css';
 
+Color('#7743CE').lighten(0.5);
 export function RoomCreationForm() {
+  const theme = useTheme();
+
+  const primaryColorLighten = Color(theme.palette.primary.main).lightness(95).hex();
+
   return (
-    <>
+    <div className="RoomCreationForm">
       <FormControl style={{ width: '100%' }}>
-        <OutlinedInput placeholder="Enter room name" />
+        <OutlinedInput style={{ background: primaryColorLighten, border: 'none' }} placeholder="Enter room name" />
       </FormControl>
       <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
         <Button
@@ -18,6 +26,6 @@ export function RoomCreationForm() {
           <b>Open Room</b>
         </Button>
       </div>
-    </>
+    </div>
   );
 }
