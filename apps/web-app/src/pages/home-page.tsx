d63c7f29344c638cb71a3d98c41821b3f1de4bd8
Asaf <asaf.karavani1@gmail.com>
2022-04-09 18:58:16 +0300

    added kube view and ingress

--- apps/web-app/src/pages/home-page.tsx
+++ apps/web-app/src/pages/home-page.tsx
@@ -5,10 +5,10 @@ import { RoomCreationForm } from '../features/rooms/room-creation-form';
 export function HomePage() {
   return (
     <div style={{ display: 'flex', alignItems: 'center', flex: 1, background: 'red', flexDirection: 'column' }}>
-      <Carousel>
-        <div>asdasd</div>
-        <div>asdasd</div>
-        <div>asdasd</div>
+      <Carousel NavButton={<div />}>
+        <div style={{ background: 'blue', width: '100%' }}>asdasd</div>
+        <div>13123</div>
+        <div>asdas123123d</div>
       </Carousel>
       <Paper style={{ padding: 20, margin: 20, width: 'calc(100% - 40px)', height: 'min-content' }} variant="outlined">
         <RoomCreationForm />
