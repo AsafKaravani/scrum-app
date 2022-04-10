43eb9b368a880117fdfa78c143ed09b4758754db
Assaf Koren <assafnoahkoren@gmail.com>
2022-04-10 17:13:36 +0300

    .

--- apps/web-app/src/pages/home-page.tsx
+++ apps/web-app/src/pages/home-page.tsx
@@ -4,9 +4,14 @@ import { RoomCreationForm } from '../features/rooms/room-creation-form';
 export function HomePage() {
   return (
     <div style={{ display: 'flex', alignItems: 'center', flex: 1, flexDirection: 'column' }}>
-      <Paper style={{ padding: 20, margin: 20, width: 'calc(100% - 40px)', height: 'min-content' }} variant="outlined">
-        <RoomCreationForm />
-      </Paper>
+      <div style={{flex: 1, width: '100%'}}>
+        
+      </div>
+      <div style={{width: '100%'}}>
+        <Paper style={{ padding: 20, margin: 20, width: 'calc(100% - 40px)', height: 'min-content' }} variant="outlined">
+          <RoomCreationForm />
+        </Paper>
+      </div>
     </div>
   );
 }
