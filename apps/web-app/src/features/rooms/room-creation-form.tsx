cf7d944a8b2c88f59b3855df473a265df26f2375
Asaf <asaf.karavani1@gmail.com>
2022-04-11 01:46:48 +0300

    .

--- apps/web-app/src/features/rooms/room-creation-form.tsx
+++ apps/web-app/src/features/rooms/room-creation-form.tsx
@@ -1,4 +1,5 @@
-import { Button, FormControl, OutlinedInput } from '@mui/material';
+import { Button, FormControl, OutlinedInput, TextField } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
 import { useTheme } from '@mui/material/styles';
 import Color from 'color';
 import './room-creation-form.css';
@@ -6,14 +7,28 @@ import './room-creation-form.css';
 Color('#7743CE').lighten(0.5);
 export function RoomCreationForm() {
   const theme = useTheme();
-
   const primaryColorLighten = Color(theme.palette.primary.main).lightness(95).hex();
 
+  const { getValues, control, handleSubmit } = useForm({
+    defaultValues: {
+      roomName: '',
+    },
+  });
+
   return (
-    <div className="RoomCreationForm">
-      <FormControl style={{ width: '100%' }}>
-        <OutlinedInput style={{ background: primaryColorLighten, border: 'none' }} placeholder="Enter room name" />
-      </FormControl>
+    <form className="RoomCreationForm">
+      <Controller
+        name="roomName"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <OutlinedInput
+            value={value}
+            onChange={onChange}
+            style={{ background: primaryColorLighten, border: 'none', width: '100%' }}
+            placeholder="Enter room name"
+          />
+        )}
+      />
       <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
         <Button
           variant="contained"
@@ -22,10 +37,13 @@ export function RoomCreationForm() {
           style={{ marginTop: 20, borderRadius: 10 }}
           size="large"
           startIcon={<i className="fa fa-screen-users" />}
+          onClick={handleSubmit(() => {
+            console.log(getValues());
+          })}
         >
           <b>Open Room</b>
         </Button>
       </div>
-    </div>
+    </form>
   );
 }
--- apps/web-app/src/pages/home-page.tsx
+++ apps/web-app/src/pages/home-page.tsx
@@ -4,11 +4,12 @@ import { RoomCreationForm } from '../features/rooms/room-creation-form';
 export function HomePage() {
   return (
     <div style={{ display: 'flex', alignItems: 'center', flex: 1, flexDirection: 'column' }}>
-      <div style={{flex: 1, width: '100%'}}>
-        
-      </div>
-      <div style={{width: '100%'}}>
-        <Paper style={{ padding: 20, margin: 20, width: 'calc(100% - 40px)', height: 'min-content' }} variant="outlined">
+      <div style={{ flex: 1, width: '100%' }}></div>
+      <div style={{ width: '100%' }}>
+        <Paper
+          style={{ padding: 20, margin: 20, width: 'calc(100% - 40px)', height: 'min-content' }}
+          variant="outlined"
+        >
           <RoomCreationForm />
         </Paper>
       </div>
