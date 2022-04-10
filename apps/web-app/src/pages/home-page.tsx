8ad8c88d489a7a15659a2397b426e99e4e39d585
Assaf Koren <assafnoahkoren@gmail.com>
2022-04-10 16:58:36 +0300

    .

--- apps/web-app/src/pages/home-page.tsx
+++ apps/web-app/src/pages/home-page.tsx
@@ -1,15 +1,9 @@
 import { Paper } from '@mui/material';
-import Carousel from 'react-material-ui-carousel';
 import { RoomCreationForm } from '../features/rooms/room-creation-form';
 
 export function HomePage() {
   return (
-    <div style={{ display: 'flex', alignItems: 'center', flex: 1, background: 'red', flexDirection: 'column' }}>
-      <Carousel NavButton={<div />}>
-        <div style={{ background: 'blue', width: '100%' }}>asdasd</div>
-        <div>13123</div>
-        <div>asdas123123d</div>
-      </Carousel>
+    <div style={{ display: 'flex', alignItems: 'center', flex: 1, flexDirection: 'column' }}>
       <Paper style={{ padding: 20, margin: 20, width: 'calc(100% - 40px)', height: 'min-content' }} variant="outlined">
         <RoomCreationForm />
       </Paper>
